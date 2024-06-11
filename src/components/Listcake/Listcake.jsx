c5a7885dcd20b217732ef0ababb94003045cd642
dinhlan17122011 <dinhlan17122011@gmail.com>
2024-06-11 20:06:30 +0700

    fpoly

--- src/components/Listcake/Listcake.jsx
+++ src/components/Listcake/Listcake.jsx
@@ -1,25 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
+import React, { useEffect, useState } from 'react';
+import './List.css';
 import axios from 'axios';
+
 const Listcake = () => {
-  const [products, setProducts] = useState();
-    useEffect(() => {
-        const api = async () =>{
-            try {
-                const res = await axios.get('http://localhost:3000/cake')
-                setProducts(res.data)
-                console.log(res.data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        api()
-    }, []);
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/cake');
+        setProducts(res.data);
+        // Extract categories
+        const uniqueCategories = [...new Set(res.data.map(product => product.catogory))];
+        setCategories(uniqueCategories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-        1
+      {categories.map((category, index) => (
+        <div key={index}>
+          <h1 className='text'>{category}</h1>
+          <div className="product-list">
+            {products.filter(product => product.catogory === category).map((item, index) => (
+              <div key={index} className="product-card">
+                <img className="product-img" src={item.img} alt={item.name} />
+                <div className="product-info">
+                  <h3 className="product-title">{item.name}</h3>
+                  <p className="product-description">{item.description}</p>
+                  <p className="product-price">Giá: <span className="old-price">{item.number}</span> đ</p>
+                  <p>id:{item._id}</p>
+                  <p>{item.catogory}</p>
+                  <p className="product-size">Kích thước: {item.size}</p>
+                  <button className="order-button">Đặt ngay</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Listcake
+export default Listcake;
